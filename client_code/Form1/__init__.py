a6ddb79f5e98f80598e066fe0ecc3dc18ee74249
omerkatz18@gmail.com <omerkatz18@gmail.com>
2026-03-18 09:45:08 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -16,3 +16,15 @@ class Form1(Form1Template):
     result = anvil.server.call('server_says_hello', client_msg)
     self.text_box_2.text = result
 
+  @handle("file_loader_1", "change")
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    pass
+
+    def file_loader_1_change(self, file, **event_args):
+      if file:  # Ensure that a file has been uploaded
+        uploaded_image = self.file_loader_1.file
+        self.image_loaded.source = uploaded_image
+        result = anvil.server.call('predict_image', uploaded_image)
+        self.prediction_text_box.text = result
+
