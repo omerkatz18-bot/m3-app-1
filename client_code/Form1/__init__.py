f0788f1fe4eaa3ff41bc4f79c3801da20a517063
omerkatz18@gmail.com <omerkatz18@gmail.com>
2026-03-26 13:56:22 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,14 +9,14 @@ class Form1(Form1Template):
     # Any code you write here will run before the form opens.
 
   @handle("button_1", "click")
-  def button_1_click(self, **event_args):
+  def  button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     client_msg = self.text_box_1.text
     result = anvil.server.call('server_says_hello', client_msg)
     self.text_box_2.text = result
 
   @handle("file_loader_1", "change")
-  def file_loader_1_change(self, file, **event_args):
+  def  file_loader_1_change(self, file, **event_args):
     if file:  # Ensure that a file has been uploaded
       uploaded_image = self.file_loader_1.file
       self.image_loaded.source = uploaded_image
