1eb845e252f704721ff26c78ddf4a43e7530701e
omerkatz18@gmail.com <omerkatz18@gmail.com>
2026-03-26 13:09:56 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,23 +1,19 @@
-import anvil.server
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.server
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
+  def _init_(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
 
-
-@handle("button_1", "click")
-def button_1_click(self, **event_args):
-  """This method is called when the button is clicked"""
-  client_msg = self.text_box_1.text
-  result = anvil.server.call('server_says_hello', client_msg)
-  self.text_box_2.text = result
-
+  @handle("button_1", "click")
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    client_msg = self.text_box_1.text
+    result = anvil.server.call('server_says_hello', client_msg)
+    self.text_box_2.text = result
 
   @handle("file_loader_1", "change")
   def file_loader_1_change(self, file, **event_args):
@@ -26,6 +22,3 @@ def button_1_click(self, **event_args):
       self.image_loaded.source = uploaded_image
       result = anvil.server.call('predict_image', uploaded_image)
       self.prediction_text_box.text = result
-
-
-
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,16 +1,1 @@
 import anvil.server
-
-@handle("file_loader_1", "change")
-def file_loader_1_change(self, file, **event_args):
-  if file:  # Ensure that a file has been uploaded
-    uploaded_image = self.file_loader_1.file
-    self.image_loaded.source = uploaded_image
-    result = anvil.server.call('predict_image', uploaded_image)
-    self.prediction_text_box.text = result
-    
-def button_1_click(self, **event_args):
-  """This method is called when the button is clicked"""
-  client_msg = self.text_box_1.text
-  result = anvil.server.call('server_says_hello', client_msg)
-  self.text_box_2.text = result
-
