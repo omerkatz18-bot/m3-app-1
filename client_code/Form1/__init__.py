722e6612e19071fff5761c8ec0c203f9b0a95e4f
omerkatz18@gmail.com <omerkatz18@gmail.com>
2026-04-23 20:17:58 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,22 +3,29 @@ from anvil import *
 import anvil.server
 
 class Form1(Form1Template):
-  def _init_(self, **properties):
+  def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
 
   @handle("button_1", "click")
-  def  button_1_click(self, **event_args):
+  def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     client_msg = self.text_box_1.text
+    print(f"Client message: {client_msg}")  # לוג להצגת ההודעה שהתקבלה
     result = anvil.server.call('server_says_hello', client_msg)
+    print(f"Received from server: {result}")  # לוג להצגת התוצאה מהשרת
     self.text_box_2.text = result
 
   @handle("file_loader_1", "change")
-  def  file_loader_1_change(self, file, **event_args):
+  def file_loader_1_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
     if file:  # Ensure that a file has been uploaded
       uploaded_image = self.file_loader_1.file
       self.image_loaded.source = uploaded_image
+
+      # בדיקה אם הפונקציה `predict_image` קיימת בשרת
+      print("Calling predict_image with uploaded image")  # לוג לבדיקת קריאה לפונקציה
       result = anvil.server.call('predict_image', uploaded_image)
+      print(f"Received prediction result: {result}")  # לוג להצגת התוצאה מהפונקציה
       self.prediction_text_box.text = result
